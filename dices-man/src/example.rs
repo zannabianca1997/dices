de6b904103dbd45e122553870dc7167fcd1326e7
zannabianca1997 <zannabianca199712@gmail.com>
2024-08-27 22:02:27 +0200

    Factored out the example processing code

---
+++ dices-man/src/example.rs
@@ -0,0 +1,122 @@
+//! Stuff to help parsing and making sense of code examples
+
+use std::{borrow::Cow, ops::Deref, str::FromStr};
+
+use lazy_regex::{regex_captures, regex_if};
+use nunny::NonEmpty;
+
+use dices_ast::{expression::Expression, matcher::Matcher, parse::parse_file, values::ValueNull};
+
+#[derive(Debug,Clone)]
+pub struct CodeExample(Box<[CodeExamplePiece]>);
+
+impl Deref for CodeExample {
+    type Target = [CodeExamplePiece];
+
+    fn deref(&self) -> &Self::Target {
+       &*self.0
+    }
+}
+
+impl FromStr for CodeExample {
+    type Err = !;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+         // parse the test
+    let mut items = vec![];
+    let mut test = s.trim_start();
+
+    while let Some((cmd, rest)) = regex_if!(
+        r"\A(?<full>[^\S\r\n]*>>>(?<start>.*)$(?<cont>(?:(?:\r\n|\n)^[^\S\r\n]*\.\.\..*$)*))"m,
+        test,
+        {
+            let cmd = if cont.is_empty() {
+                Cow::Borrowed(start)
+            } else {
+                let mut cmd = start.to_owned();
+                for line in cont.trim_start().lines() {
+                    cmd.push_str(line.trim_start().strip_prefix("...").unwrap())
+                }
+                Cow::Owned(cmd)
+            };
+            (
+                CodeExampleCommand {
+                    ignore: false,
+                    command: parse_file(&cmd).expect("Cannot parse command"),
+                },
+                &test[full.len()..],
+            )
+        }
+    )
+    .or_else(|| {
+        // try to capture an ignored command
+        regex_if!(
+            r"\A(?<full>[^\S\r\n]*#[^\S\r\n]*>>>(?<start>.*)$(?<cont>(?:(?:\r\n|\n)^[^\S\r\n]*#[^\S\r\n]*\.\.\..*)$)*)"m,
+            test,
+            {
+                let cmd = if cont.is_empty() {
+                    Cow::Borrowed(start)
+                } else {
+                    let mut cmd = start.to_owned();
+                    for line in cont.lines() {
+                        cmd.push_str(line.trim_start().strip_prefix('#').unwrap().trim_start().strip_prefix("...").unwrap())
+                    }
+                    Cow::Owned(cmd)
+                };
+                (
+                    CodeExampleCommand {
+                        ignore: true,
+                        command: parse_file(&cmd).expect("Cannot parse command"),
+                    },
+                    &test[full.len()..],
+                )
+            }
+        )
+    }) {
+        // need now to split the result
+        let (_,res) = regex_captures!(r"\A((?:.|\n)*?)(?:^[^\S\r\n]*(?:#[^\S\r\n]*)?>>>|\z)"m,rest).expect("The regex is infallible");
+        test = &rest[res.len()..];
+        // conversting the result
+        let res = res.trim();
+        let res = if res.starts_with('#') {
+            for l in res
+            .lines()
+            .filter(|l| 
+                !l
+                .trim()
+                .is_empty()
+            ) {
+                assert!(l.trim_start().starts_with('#'), "Inconsistent ignoring of result lines")
+            }
+            None
+        } else {
+            if res == "" {
+                // empty result corresponds to empty values
+                Some(Matcher::Exact(ValueNull.into()))
+            } else {
+                Some(res.parse().expect("The value must be a valid result matcher"))
+            }
+        };
+        items.push(CodeExamplePiece {cmd, res})
+    }
+    assert_eq!(test.trim(), "", "Cannot recognize command prompt");
+
+    Ok(Self(items.into_boxed_slice()))
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct CodeExamplePiece {
+   pub cmd: CodeExampleCommand,
+   pub res: Option<Matcher>,
+}
+
+#[derive(Debug,Clone)]
+pub struct CodeExampleCommand {
+    /// Do not check the result of this command
+    ///
+    /// Used to do setup stuff, as it is not printed
+   pub ignore: bool,
+    /// The actual command
+   pub command: Box<NonEmpty<[Expression]>>,
+}
