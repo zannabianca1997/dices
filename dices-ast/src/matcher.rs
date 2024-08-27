04b3299dc3b011dc00810ce619136aea99fac8e4
zannabianca1997 <zannabianca199712@gmail.com>
2024-08-27 21:07:32 +0200

    moved example parsing to the manual crate

--- dices-ast/src/matcher.rs
+++ dices-ast/src/matcher.rs
@@ -4,6 +4,7 @@ use either::Either::{Left, Right};
 use peg::{error::ParseError, str::LineCol};
 use std::{borrow::Cow, collections::BTreeMap, str::FromStr};
 
+#[derive(Debug, Clone)]
 pub enum Matcher {
     Exact(Value),
     List(Box<[Matcher]>),
--- dices-mantest/src/lib.rs
+++ dices-mantest/src/lib.rs
@@ -1,5 +1,4 @@
 //! Testing for the examples provided in the manual
-#![feature(iter_intersperse)]
 #![feature(never_type)]
 
 #[cfg(test)]
