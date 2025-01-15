529f40551c54e9888bbed702b91e3335fa8c21b8
zannabianca1997 <zannabianca199712@gmail.com>
2025-01-15 01:32:57 +0100

    fix: removed server code
    The server feature is not ready to be released on the main branch

--- server/src/main.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-use dices_server::{CliArgs, MainError};
-
-#[tokio::main]
-async fn main() -> Result<(), MainError> {
-    let args = CliArgs::parse();
-    dices_server::main(args).await.map(drop)
-}
