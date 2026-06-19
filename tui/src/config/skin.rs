2e7715cf1786b0e7fd533972e9869de8ffe9242d
zannabianca1997 <zannabianca199712@gmail.com>
2026-06-20 01:30:58 +0200

    Fix: color being forced by default config

--- tui/src/config/skin.rs
+++ tui/src/config/skin.rs
@@ -5,12 +5,16 @@ use serde::{Deserialize, Serialize};
 use super::theme::Theme;
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(default)]
 pub struct Skin {
     /// If opening and closing banners are shown
+    #[serde(skip_serializing_if = "is_true")]
     pub banners: bool,
     /// Use non-ascii character
+    #[serde(skip_serializing_if = "is_true")]
     pub graphical: bool,
     /// Colorize the output
+    #[serde(skip_serializing_if = "is_true")]
     pub color: bool,
     /// Theme chosen
     pub theme: Theme,
@@ -26,4 +30,11 @@ impl Default for Skin {
             theme: Theme::default(),
         }
     }
+}
+
+fn is_true(x: &bool) -> bool {
+    *x
+}
+fn is_false(x: &bool) -> bool {
+    !x
 }
